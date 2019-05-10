72e6ed752656ef18304fdffbf5dd08c647c3f1d4
Adrian Hoyos <ahoyosmarqu@uniminuto.edu.co>
2019-05-10 00:24:35 -0500

    creacion de interfaz Usuario

--- src/com/hackersnews/idao/INoticeDao.java
+++ src/com/hackersnews/idao/INoticeDao.java
@@ -5,15 +5,28 @@ import java.util.List;
 import com.hackersnews.model.Notice;
 import com.hackersnews.model.User;
 
+/**
+ * @author Alexis Holguin github:MoraHol
+ *
+ */
 public interface INoticeDao {
 	public int save(Notice notice);
+
 	public int update(Notice notice);
+
 	public int delete(int id);
+
 	public Notice findNoticeById(int id);
+
 	public List<Notice> findNoticesByUser(User user);
+
 	public List<Notice> findAllNotices();
+
 	public int findPointByNotice(Notice notice);
+
 	public int rateNotice(Notice notice);
-	public int removePoint(User user,Notice notice);
-	public List<Notice> findNoticesRatedByUser(User user);	
+
+	public int removePoint(User user, Notice notice);
+
+	public List<Notice> findNoticesRatedByUser(User user);
 }
--- src/com/hackersnews/idao/IUserDao.java
+++ src/com/hackersnews/idao/IUserDao.java
@@ -4,14 +4,17 @@
 package com.hackersnews.idao;
 
 import java.util.List;
-
-import com.hackersnews.model.Notice;
-import com.hackersnews.model.User;;
+import com.hackersnews.model.User;
 
 /**
  * @author Alexis Holguin github:MoraHol
  *
  */
 public interface IUserDao {
+	public int save(User user);
+	public int update(User user);
+	public int delete(int id);
+	public User findUserById(int id);
+	public List<User> findAll();
 	
 }
